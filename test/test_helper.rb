c9229b27a512d7a7ede1b516c1b606e71d4cc15e
Ubuntu <ubuntu@ip-172-31-34-15.ap-northeast-1.compute.internal>
2023-08-22 14:17:21 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,10 @@ class ActiveSupport::TestCase
 
   # test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
   fixtures :all
+  
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # （すべてのテストで使うその他のヘルパーメソッドは省略）
 end
